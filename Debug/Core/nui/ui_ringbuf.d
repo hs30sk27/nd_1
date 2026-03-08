2bf1e15a70495e8d589bacdbdbac84ad773ca5d8
unknown <hs30sk27@gmail.com>
2026-03-08 23:52:37 +0900

    Remove directory from remote repository

--- Debug/Core/nui/ui_ringbuf.d
+++
@@ -1,2 +0,0 @@
-Core/nui/ui_ringbuf.o: ../Core/nui/ui_ringbuf.c ../Core/nui/ui_ringbuf.h
-../Core/nui/ui_ringbuf.h:
--- Debug/Core/ui/ui_crc.d
+++
@@ -1,2 +0,0 @@
-Core/ui/ui_crc.o: ../Core/ui/ui_crc.c ../Core/ui/ui_crc.h
-../Core/ui/ui_crc.h:
