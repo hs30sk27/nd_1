f8425789ed2062c65b4d78c340557630b2305212
unknown <hs30sk27@gmail.com>
2026-03-08 23:51:45 +0900

    Remove directory from remote repository

--- Debug/Core/nui/ui_fault.d
+++
@@ -1,2 +0,0 @@
-Core/nui/ui_fault.o: ../Core/nui/ui_fault.c ../Core/nui/ui_fault.h
-../Core/nui/ui_fault.h:
--- Debug/Core/ui/ui_rf.d
+++
@@ -1,4 +0,0 @@
-Core/ui/ui_rf.o: ../Core/ui/ui_rf.c ../Core/ui/ui_rf.h \
- ../Core/ui/ui_conf.h
-../Core/ui/ui_rf.h:
-../Core/ui/ui_conf.h:
